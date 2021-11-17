6f10a93af17ab7761ced1af8327801a39d3e1d42
jiveshbishtOFFICIAL <89592379+jiveshbishtOFFICIAL@users.noreply.github.com>
2021-11-17 06:08:06 +0100

    Update PC_Miner.py

--- PC_Miner.py
+++ PC_Miner.py
@@ -179,8 +179,7 @@ class Client:
                     NODE_PORT = response["port"]
                     return (NODE_ADDRESS, NODE_PORT)
                 elif "message" in response:
-                    pretty_print(f"Warning: {response['message']}"
-                                 + ", retrying in 15s", "warning", "net0")
+                    pretty_print(f"Warning: {response['message']}" + ", retrying in 15s", "warning", "net0")
                     sleep(10)
                 else:
                     raise Exception(
